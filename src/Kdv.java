97d283bbded9b234fe7a615aa0ad12d47cada255
ebbrusena <ebrusenaozsogut@gmail.com>
2025-10-05 12:26:49 +0300

    ilk commit

---
+++ src/Kdv.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args){
+        double tutar, kdvOran = 0.18;
+        Scanner input = new Scanner(System.in);
+        System.out.println("tutarı giriniz: ");
+
+        tutar = input.nextDouble();
+        double kdvTutar = tutar * kdvOran;
+
+        double kdvliTutar = tutar + kdvTutar;
+        System.out.println("kdv oranı: " kdvOran);
+        System.out.println("kdv tutarı:" kdvTutar);
+        System.out.println("kdvli tutar:" kdvliTutar);
+        System.out.println(kdvliTutar);
+
+    }
+
+}
